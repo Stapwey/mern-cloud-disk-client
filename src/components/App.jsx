f811e52a53434d3c160cbfd16d20c76e7e22ad3e
Dmitriy Semenov <semenov.ds12@gmail.com>
2023-05-11 15:10:52 +0300

    add disk page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect} from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Registration from "./authorization/Registration";
+import Disk from "./disk/Disk";
 import Login from "./authorization/Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "../actions/user";
@@ -21,10 +22,19 @@ function App() {
             <div className='app'>
                 <Navbar/>
                 <div className="wrap">
-                    {!isAuth &&
+                    {!isAuth ?
                         <Routes>
                             <Route path='/registration' element={<Registration/>} />
                             <Route path="/login" element={<Login/>}/>
+                            <Route
+                                path="*"
+                                element={<Navigate to="/login" replace />}
+                            />
+                        </Routes>
+                        :
+                        <Routes>
+                            <Route exact path='/' element={<Disk/>} />
+                            <Route path="*" element={<Navigate to="/" replace />} />
                         </Routes>
                     }
                 </div>

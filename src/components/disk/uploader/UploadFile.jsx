c729577329b53a5765a965730615a6e778f9d427
Dmitriy Semenov <semenov.ds12@gmail.com>
2023-05-12 16:53:06 +0300

    add avatar

--- src/components/disk/uploader/UploadFile.jsx
+++ src/components/disk/uploader/UploadFile.jsx
@@ -10,7 +10,7 @@ const UploadFile = ({file}) => {
         <div className="upload-file">
             <div className="upload-file__header">
                 <div className="upload-file__name">{file.name}</div>
-                <button className="upload-file__remove" onClick={() => dispatch(removeUploadFile(file.id))}>X</button>
+                <button className="upload-file__remove" onClick={() => dispatch(removeUploadFile(file.id))}></button>
             </div>
             <div className="upload-file__progress-bar">
                 <div className="upload-file__upload-bar" style={{width: file.progress + "%"}}/>
